3058b82a44530fee0f9da21e0a6d8dc8ca0b21b2
Artyom <artyom-first@yandex.ru>
2022-10-25 19:13:21 +0500

    fix: убрал .env

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -103,8 +103,9 @@ module.exports.login = (req, res, next) => {
 
   User.findUserByCredentials(email, password)
     .then((user) => {
-      const token = jwt.sign({ _id: user._id }, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret', { expiresIn: '7d' });
-      // const token = jwt.sign({ _id: user._id }, 'super-secret-key', { expiresIn: '7d' });
+    // eslint-disable-next-line max-len
+    // const token = jwt.sign({ _id: user._id }, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret', { expiresIn: '7d' });
+      const token = jwt.sign({ _id: user._id }, 'dev-secret', { expiresIn: '7d' });
       res.send({ token });
     })
     .catch((err) => {
