d68bdc1967ba8626d5ca76a419c1ca39ea6bb8c4
Artyom <artyom-first@yandex.ru>
2022-10-26 20:16:16 +0500

    fix: headers отдельно

--- backend/app.js
+++ backend/app.js
@@ -25,9 +25,7 @@ const corsOptions = { // настройки КОРС-а
     'http://your-mesto.nomoredomains.icu',
   ],
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-  headers: {
-    'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
-  },
+  headers: 'Origin, X-Requested-With, Content-Type, Accept',
   preflightContinue: false,
 };
 app.use(cors(corsOptions));
